4984e4848fcb01103a3c935d0917dda6f215b8e1
Ahmad hassan <ahmadhassan136@gmail.com>
2023-07-03 17:27:14 +0500

    Creating outlets

--- Clima/ViewController.swift
+++ Clima/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var temperature : UILabel!
+    @IBOutlet var city : UILabel!
+    @IBOutlet var conition : UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
